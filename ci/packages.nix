1a2fedcb5a2c54dd1e8402230d37d18fe2a55336
arcnmx <arcnmx@users.noreply.github.com>
2019-04-11 17:52:46 -0700

    allow packages to opt out of ci tests

--- ci/packages.nix
+++ ci/packages.nix
@@ -1,6 +1,7 @@
 { arc }:
 let
   packages = (builtins.attrValues arc.packages.select.derivations);
-  filter = pkg: pkg ? meta.broken -> !pkg.meta.broken;
+  filter = pkg: (pkg ? meta.broken -> !pkg.meta.broken) &&
+    (pkg ? meta.skip.ci -> !pkg.meta.skip.ci);
 in
 builtins.filter filter packages
