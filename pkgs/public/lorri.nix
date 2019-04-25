36ad7eb5fe7eaa99d7fa54467fec63ca9fad3e69
arcnmx <arcnmx@users.noreply.github.com>
2019-04-25 14:23:32 -0700

    build lorri normally

--- pkgs/public/lorri.nix
+++ pkgs/public/lorri.nix
@@ -1,4 +1,4 @@
-{ arc, pkgs, fetchFromGitHub, rustPlatform }: let
+{ lib, stdenv, fetchFromGitHub, rustPlatform, coreutils, nix, direnv, which, darwin }: rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "target";
     repo = "lorri";
@@ -6,8 +6,17 @@
     rev = "094a903d19eb652a79ad6e7db6ad1ee9ad78d26c";
     sha256 = "0y9y7r16ki74fn0xavjva129vwdhqi3djnqbqjwjkn045i4z78c8";
   };
-in (import "${src}/default.nix" {
-  inherit pkgs src;
-}).overrideAttrs (_: if !arc.lib.isRust2018 rustPlatform then {
-  meta.broken = true;
-} else { })
+  name = "lorri";
+  cargoSha256 = "0lx4r05hf3snby5mky7drbnp006dzsg9ypsi4ni5wfl0hffx3a8g";
+  COREUTILS = coreutils;
+  BUILD_REV_COUNT = 1;
+
+  buildInputs = [ nix direnv which ] ++
+    lib.optionals stdenv.isDarwin [
+      darwin.cf-private
+      darwin.Security
+      darwin.apple_sdk.frameworks.CoreServices
+    ];
+
+  doCheck = false;
+}
