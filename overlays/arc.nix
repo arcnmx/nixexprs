9d53e772b2d2cd35a575b2bf49c58b2a080efcd9
arcnmx <arcnmx@users.noreply.github.com>
2019-05-05 23:41:32 -0700

    ax88179_178a kernel module

--- overlays/arc.nix
+++ overlays/arc.nix
@@ -10,5 +10,8 @@ self: super: let
     lib = (super.lib or {}) // arc.lib;
     vimPlugins = super.vimPlugins // arc.packages.select.vimPlugins;
     gitAndTools = super.gitAndTools // arc.packages.select.gitAndTools;
+    linuxPackagesFor = kernel: (super.linuxPackagesFor kernel).extend (super.lib.const (ksuper: {
+      ax88179_178a = arc.ax88179_178a.override { linux = ksuper.kernel; };
+    }));
   } // arc.packages.select.public // arc.lib.build-support;
 in overlay
