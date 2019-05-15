7949c2db1227a09bca3acf00bd236a9d148f83eb
arcnmx <arcnmx@users.noreply.github.com>
2019-05-15 15:56:07 -0700

    rust-analyzer

--- pkgs/public/crates/default.nix
+++ pkgs/public/crates/default.nix
@@ -1,2 +1,17 @@
 { callPackage }: callPackage {
+  rust-analyzer = { fetchFromGitHub, rustPlatform }: rustPlatform.buildRustPackage rec {
+    pname = "rust-analyzer";
+    version = "bebc5c71664a144b9addd357eb503f776f2cf416";
+    src = fetchFromGitHub {
+      owner = "rust-analyzer";
+      repo = "rust-analyzer";
+      rev = version;
+      sha256 = "0aj5lzilayxjrhmnswciki37inn07p1z6ksf3kr65icqvs7b6w0s";
+    };
+    cargoBuildFlags = ["--features" "jemalloc" "-p" "ra_lsp_server"];
+
+    cargoSha256 = "11h60kcf2g050w97lkc7dwdinrbay459q2ccyw3cxl8620p95a8v";
+
+    doCheck = false;
+  };
 } { }
