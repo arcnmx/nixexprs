8e7f79632ff27ff18ec7cf7701d9faf695fbd2fa
arcnmx <arcnmx@users.noreply.github.com>
2019-07-18 18:16:15 -0700

    adjust go module hash for 19.03

--- pkgs/public/matrix/mautrix-whatsapp/default.nix
+++ pkgs/public/matrix/mautrix-whatsapp/default.nix
@@ -1,4 +1,4 @@
-{ buildGoModule, fetchFromGitHub }: buildGoModule rec {
+{ buildGoModule, fetchFromGitHub, lib }: buildGoModule rec {
   pname = "mautrix-whatsapp";
   version = "2019-07-04";
   src = fetchFromGitHub {
@@ -8,5 +8,8 @@
     sha256 = "12209m3x01i7bnnkg57ag1ivsk6n6pqaqfin7y02irgi3i3rm31r";
   };
 
-  modSha256 = "151b4xxzj8z1avq3d7p1vyvr3lh1fk130clpf59ahxrgycklrif1";
+  modSha256 =
+    if lib.versionOlder lib.version "19.09pre"
+    then "0djvbgqam0s8hcyrd0585i6h5da1g50cy8xhqh78i7d7cl28yvsp"
+    else "151b4xxzj8z1avq3d7p1vyvr3lh1fk130clpf59ahxrgycklrif1";
 }
