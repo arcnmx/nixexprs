e3d57fa0524398937be1913c4c4108752b29c54a
arcnmx <arcnmx@users.noreply.github.com>
2019-07-30 13:21:55 -0700

    bridges that probably won't stay here for very long

--- pkgs/public/matrix/default.nix
+++ pkgs/public/matrix/default.nix
@@ -1,4 +1,6 @@
 {
+  matrix-puppet-skype = import ./matrix-puppet-skype;
   mautrix-hangouts = import ./mautrix-hangouts;
+  matrix-appservice-discord = import ./matrix-appservice-discord;
   matrix-bifrost = import ./matrix-bifrost;
 }
