23184c2d90d6e2b6867de6a651d7b3606acf36b4
arcnmx <arcnmx@users.noreply.github.com>
2019-08-27 14:09:28 -0700

    cargo-{call-stack,stack-sizes}

--- pkgs/public/crates/default.nix
+++ pkgs/public/crates/default.nix
@@ -66,4 +66,42 @@
       fi
     '';
   };
+
+  cargo-call-stack = {
+    lib, fetchFromGitHub, rustPlatform
+  }: rustPlatform.buildRustPackage rec {
+    pname = "cargo-call-stack";
+    version = "0.1.3";
+
+    src = fetchFromGitHub {
+      owner = "japaric";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "0bbkvxb0y8czidvmsrnk46gm7r8da7cckdbkwxwby2bcvv2fg812";
+    };
+
+    cargoPatches = [ ./cargo-call-stack-lock.patch ];
+    cargoSha256 = if lib.isNixpkgsStable
+      then lib.fakeSha256
+      else "0v2zgbzx3sxmwn6axiv1nz59y62sfzdkfbs90ly63zcbjf801qm9";
+  };
+
+  cargo-stack-sizes = {
+    lib, fetchFromGitHub, rustPlatform
+  }: rustPlatform.buildRustPackage rec {
+    pname = "stack-sizes";
+    version = "0.4.0";
+
+    src = fetchFromGitHub {
+      owner = "japaric";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "0k260hkv734zwwwz5r93zriimrg13v4h0cmhmqf5a4svkns8z06h";
+    };
+
+    cargoPatches = [ ./cargo-stack-sizes-lock.patch ];
+    cargoSha256 = if lib.isNixpkgsStable
+      then lib.fakeSha256
+      else "11snk7mah1kvvxjlvcx8xz2h1f4ficyd8czh43whhn0y1lsb0k57";
+  };
 }
