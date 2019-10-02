035fd210b2899ff2c2c6da8f01e478ccdb30228c
arcnmx <arcnmx@users.noreply.github.com>
2019-10-01 17:09:32 -0700

    move CI to modules

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 CI_URL=https://github.com/arcnmx/ci/archive/allnix.tar.gz
+CI_URL=/home/arc/projects/arc.github/ci
 exec nix run -L --show-trace -f $CI_URL \
-	--arg config ./ci/config.nix test.all -c ci-build "$@"
+	--arg config ./ci/config.nix stage.stable.test -c ci-build "$@"
--- ci/config.nix
+++ ci/config.nix
@@ -1,67 +1,126 @@
-{ ci }: let
-  pkgs = ci.channels.nixpkgs;
-in {
+{ lib, config, pkgs, import, ... }: with lib; {
   # https://github.com/arcnmx/ci
-  channels = {
-    nixpkgs = "unstable";
-    home-manager = "master";
-    mozilla = "master";
-  } // ci.channelsFromEnv ci.screamingSnakeCase "NIX_CHANNELS_";
+  gh-actions = {
+    jobs = optionalAttrs config.ci.gh-actions.emit {
+      ${config.ci.gh-actions.id}.env = {
+        GITHUB_EVENT_NAME = "\${{ github.event_name }}";
+      };
+    };
+    on = {
+      push = {};
+      pull_request = {};
+      schedule = [ {
+        cron = "30 3-7/2,13-21/4 * * *";
+      } ];
+    };
+  };
+  ci = {
+    configPath = "./ci/config.nix";
+    gh-actions = {
+      path = ".github/workflows/build.yml";
+      enable = true;
+    };
+    env = {
+      channels = {
+        nixpkgs = mkDefault "unstable";
+        home-manager = mkDefault "master";
+      };
+      cache.cachix.arc.enable = true;
+    };
 
-  allowRoot = (builtins.getEnv "CI_ALLOW_ROOT") != "";
-  closeStdin = (builtins.getEnv "CI_CLOSE_STDIN") != "";
+    project = {
+      name = "arc-nixexprs";
+      tasks = let
+        arc = import ../. { inherit pkgs; };
+        cipkgs = config.ci.env.bootstrap.pkgs;
+        channel = cipkgs.nix-gitignore.gitignoreSourcePure [ ../.gitignore ''
+          /ci/
+          /README.md
+          /.gitmodules
+          /.github
+          /.azure
+        '' ] ../.;
+      in {
+        eval = {
+          inputs = with cipkgs; let
+            eval = attr: cipkgs.mkCiCommand {
+              pname = "eval-${attr}";
+              displayName = "nix eval ${attr}";
+              timeout = 60;
+              src = channel;
 
-  #glibcLocales = [ pkgs.glibcLocales ];
-
-  cache.cachix = ci.cachixFromEnv { };
-
-  tasks = let
-    arc = import ../. { inherit pkgs; };
-    channel = ci.cipkgs.nix-gitignore.gitignoreSourcePure [ ../.gitignore ''
-      /ci/
-      /README.md
-      /.gitmodules
-      /.azure
-    '' ] ../.;
-  in {
-    eval = ci.mkCiTask {
-      pname = "eval";
-      inputs = with ci.cipkgs; with ci.env; let
-        eval = attr: ci.mkCiCommand {
-          pname = "eval-${attr}";
-          displayName = "nix eval ${attr}";
-          timeout = 60;
-          src = channel;
-
-          nativeBuildInputs = [ nix ];
-          command = "nix eval -f $src/default.nix ${attr}";
-          hostExec = true; # nix doesn't work inside builders ("recursive nix")
+              nativeBuildInputs = [ nix ];
+              command = "nix eval -f $src/default.nix ${attr}";
+              hostExec = true; # nix doesn't work inside builders ("recursive nix")
+            };
+          in [ (eval "lib") (eval "modules") (eval "overlays") ];
         };
-      in [ (eval "lib") (eval "modules") (eval "overlays") ];
-    };
-    build = ci.mkCiTask {
-      pname = "build";
-      inputs = builtins.attrValues arc.packages;
-      timeout = 60 * 180; # max 360 on azure
-    };
-    shells = ci.mkCiTask {
-      pname = "shells";
-      inputs = with arc.shells.rust; [ stable nightly ];
-      timeout = 60 * 90;
-    };
-    tests = ci.mkCiTask {
-      pname = "tests";
-      inputs = import ./tests.nix { inherit arc; };
-    };
-    modules = ci.mkCiTask {
-      pname = "modules";
-      displayName = "nix test modules";
-      inputs = import ./modules.nix { inherit (arc) pkgs; };
-      depends = [ ci.config.tasks.eval ];
-      cache = { wrap = true; };
-      skip = if builtins.getEnv "BUILD_REASON" == "Schedule" then "scheduled build"
-        else if ci.config.channels.home-manager != "master" then "home-manager release channel"
-        else false;
+        build = {
+          inputs = builtins.attrValues arc.packages;
+          timeoutSeconds = 60 * 180; # max 360 on azure
+        };
+        shells = {
+          inputs = with arc.shells.rust; [ stable nightly ];
+          timeoutSeconds = 60 * 90;
+        };
+        tests = {
+          inputs = import ./tests.nix { inherit arc; };
+        };
+        modules = {
+          name = "nix test modules";
+          inputs = import ./modules.nix { inherit (arc) pkgs; };
+          # TODO: depends = [ config.ci.project.tasks.eval ];
+          cache = { wrap = true; };
+          skip = if builtins.getEnv "GITHUB_EVENT_NAME" == "schedule" then "scheduled build"
+            else if config.ci.env.channels.home-manager.version != "master" then "home-manager release channel"
+            else false;
+        };
+      };
+      stages = {
+        stable = {
+          ci.pkgs.system = "x86_64-linux";
+          ci.env.channels = {
+            nixpkgs = "19.03";
+            home-manager = "release-19.03";
+          };
+        };
+        beta = {
+          ci.pkgs.system = "x86_64-linux";
+          ci.env.channels.nixpkgs = "19.09";
+        };
+        unstable = {
+          ci.pkgs.system = "x86_64-linux";
+          ci.env.channels.nixpkgs = "unstable";
+        };
+        unstable-small = {
+          ci.pkgs.system = "x86_64-linux";
+          ci.env.channels.nixpkgs = "unstable-small";
+          ci.warn = true;
+        };
+        unstable-nixpkgs = {
+          ci.pkgs.system = "x86_64-linux";
+          ci.env.channels.nixpkgs = "nixpkgs-unstable";
+          ci.warn = true;
+        };
+        stable-mac = {
+          ci.pkgs.system = "x86_64-darwin";
+          ci.env.channels = {
+            nixpkgs = "19.03";
+            home-manager = "release-19.03";
+          };
+          ci.warn = true;
+        };
+        beta-mac = {
+          ci.pkgs.system = "x86_64-darwin";
+          ci.env.channels.nixpkgs = "19.09";
+          ci.warn = true;
+        };
+        unstable-mac = {
+          ci.pkgs.system = "x86_64-darwin";
+          ci.env.channels.nixpkgs = "unstable";
+          ci.warn = true;
+        };
+      };
     };
   };
 }
