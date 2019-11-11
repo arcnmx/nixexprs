e18d3a19b7b5590d3e2e4eb9ce218affc72f0ce4
arcnmx <arcnmx@users.noreply.github.com>
2019-11-10 17:04:39 -0800

    konawall depends on grep

--- pkgs/personal/konawall/default.nix
+++ pkgs/personal/konawall/default.nix
@@ -1,6 +1,6 @@
 {
   wrapShellScriptBin,
-  coreutils, inetutils, curl, jq, xrandr ? null, feh ? null, xsetroot ? null, sway ? null,
+  coreutils, inetutils, curl, jq, gnugrep ? null, xrandr ? null, feh ? null, xsetroot ? null, sway ? null,
   hostPlatform, lib,
   swaySupport ? hostPlatform.isLinux,
   xorgSupport ? hostPlatform.isLinux
@@ -11,6 +11,6 @@ assert xorgSupport -> (feh != null && xsetroot != null && xrandr != null);
 
 wrapShellScriptBin "konawall" ./konawall.sh {
   depsRuntimePath = with lib; [coreutils inetutils curl jq] ++
-    optionals xorgSupport [feh xsetroot xrandr] ++
+    optionals xorgSupport [feh xsetroot xrandr gnugrep] ++
     optionals swaySupport [sway];
 }
