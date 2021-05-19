9b09535ac236d9653de6fe26fee0d960ada07c54
arcnmx <arcnmx@users.noreply.github.com>
2021-05-19 14:32:44 -0700

    dead for now sorry

--- pkgs/public/matrix/default.nix
+++ pkgs/public/matrix/default.nix
@@ -1,5 +1,4 @@
 {
-  matrix-puppet-skype = import ./matrix-puppet-skype;
   mx-puppet-discord = import ./mx-puppet-discord;
   mautrix-hangouts = import ./mautrix-hangouts;
   matrix-bifrost = import ./matrix-bifrost;
