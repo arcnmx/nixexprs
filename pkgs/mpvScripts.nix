3a9c1ceebb8bf4609e289037f124df4e9d291dea
arcnmx <arcnmx@users.noreply.github.com>
2021-05-30 13:30:39 -0700

    silly mpv pause indicator script is bad

--- pkgs/mpvScripts.nix
+++ pkgs/mpvScripts.nix
@@ -23,4 +23,18 @@
       description = "Automatically hides and shows OSC based upon pause status";
     };
   };
+  pause-indicator = { stdenvNoCC, fetchurl }: stdenvNoCC.mkDerivation rec {
+    pname = "mpv-pause-indicator";
+    version = "2018-04-28";
+    src = fetchurl {
+      url = "https://gist.githubusercontent.com/torque/9dbc69543118347d2e5f43239a7e609a/raw/bd9fcfe68a4f13b655c686e5790cbd2ee9489475/pause-indicator.lua";
+      sha256 = "1x219m62iljxdsk18apaf1ccf4waqdwr9gilrfj64g4j6h7k1fak";
+    };
+
+    buildCommand = ''
+      install -Dm644 $src $out/share/mpv/scripts/$scriptName
+    '';
+
+    scriptName = "pause-indicator.lua";
+  };
 }
