298f007bde9d248ec7ac714183c535cede714c90
arcnmx <arcnmx@users.noreply.github.com>
2021-07-01 15:41:34 -0700

    mark awscli2-dependent package broken

--- pkgs/personal/filebin/default.nix
+++ pkgs/personal/filebin/default.nix
@@ -1,7 +1,9 @@
 {
-  substituteShellScriptBin,
+  substituteShellScriptBin, lib,
   coreutils, awscli2, curl ? null
 }:
 substituteShellScriptBin "filebin" ./filebin.sh {
   depsRuntimePath = [coreutils awscli2 curl];
+
+  meta.broken = lib.isNixpkgsUnstable; # awscli2 broken atm
 }
