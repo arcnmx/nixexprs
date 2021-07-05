37018fc07b2d01440b37035fcd8ccde9f4a9e4a8
arcnmx <arcnmx@users.noreply.github.com>
2021-07-05 16:56:24 -0700

    removing some upstreamed packages

--- pkgs/public/default.nix
+++ pkgs/public/default.nix
@@ -2,10 +2,7 @@
   i3gopher = import ./i3gopher;
   tamzen = import ./tamzen.nix;
   paswitch = import ./paswitch.nix;
-  LanguageClient-neovim = import ./language-client-neovim.nix;
   base16-shell = import ./base16-shell.nix;
-  efm-langserver = import ./efm-langserver;
-  markdownlint-cli = import ./markdownlint-cli;
   clip = import ./clip.nix;
   nvflash = import ./nvflash.nix;
   nvidia-vbios-vfio-patcher = import ./nvidia-vbios-vfio-patcher;
--- pkgs/public/matrix/default.nix
+++ pkgs/public/matrix/default.nix
@@ -1,5 +1,3 @@
 {
-  mx-puppet-discord = import ./mx-puppet-discord;
   mautrix-hangouts = import ./mautrix-hangouts;
-  matrix-bifrost = import ./matrix-bifrost;
 }
