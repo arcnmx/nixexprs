9604d5054456228c4847c7aa64aac29743428a8d
arcnmx <arcnmx@users.noreply.github.com>
2021-07-24 16:40:45 -0700

    WIP: this shouldn't need to be explicitly set

--- ci/config.nix
+++ ci/config.nix
@@ -34,7 +34,7 @@ in {
     nixpkgs = {
       version = mkDefault "unstable";
       nixPathImport = skipModules == false;
-      args.config.checkMetaRecursively = true;
+      #args.config.checkMetaRecursively = true;
     };
     home-manager = mkDefault "master";
   };
