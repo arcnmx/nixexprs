c07e370cc59fcd3c8b4870c61b2acbbbf9b5be23
arcnmx <arcnmx@users.noreply.github.com>
2021-08-31 20:50:28 -0700

    lib.json.type

--- lib/default.nix
+++ lib/default.nix
@@ -138,6 +138,7 @@
 
   gst = import ./gst.nix { inherit lib; };
   alsa = import ./alsa.nix { inherit lib; };
+  json = import ./json.nix { inherit lib; };
 
   # NOTE: a very basic/incomplete parser
   fromYAML = import ./from-yaml.nix lib;
