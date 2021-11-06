71c4847cd0c2409e12c8c9586db4c446a3e929ee
arcnmx <arcnmx@users.noreply.github.com>
2021-11-06 10:49:54 -0700

    wireplumber-scripts update

--- pkgs/public/wireplumber/scripts.nix
+++ pkgs/public/wireplumber/scripts.nix
@@ -1,12 +1,12 @@
 { stdenvNoCC, lua-amalg, fetchFromGitHub, lua5_4 ? lua5_3, lua5_3 }: stdenvNoCC.mkDerivation {
   pname = "wireplumber-scripts";
-  version = "2021-10-26";
+  version = "2021-10-31";
 
   src = fetchFromGitHub {
     owner = "arcnmx";
     repo = "wireplumber-scripts";
-    rev = "3ff6e735e08cf84add9230f6a129391db1984926";
-    sha256 = "17bhpyjw42c7y8237mf1r3z5lsczax3i7sdsiv93nv5ic35ch6g2";
+    rev = "89c298039b685c88a6f016438b7da0883d6511d8";
+    sha256 = "0ikjxxlwzx3w5v1dwjd90v76fbq4hc5m4w9v444v094n5c9k5jyi";
   };
 
   nativeBuildInputs = [ lua-amalg ];
