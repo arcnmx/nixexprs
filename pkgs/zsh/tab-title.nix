5e9090c20bed9f02c28d45e49c26ab2b94ab33e0
arcnmx <arcnmx@users.noreply.github.com>
2021-12-01 17:08:28 -0800

    update and patch zsh-tab-title

--- pkgs/zsh/tab-title.nix
+++ pkgs/zsh/tab-title.nix
@@ -1,12 +1,16 @@
-{ fetchFromGitHub, buildZshPlugin }: buildZshPlugin rec {
+{ fetchFromGitHub, buildZshPlugin, fetchpatch }: buildZshPlugin rec {
   pname = "zsh-tab-title";
-  pluginName = "title";
-  version = "2.2.0";
+  version = "2.3.1";
 
   src = fetchFromGitHub {
     owner = "trystan2k";
     repo = pname;
     rev = "v${version}";
-    sha256 = "0wpin0lv419zqijx5vjckk785gm2sqn36qyncwvsryi6n5qi7vij";
+    sha256 = "137mfwx52cg97qy3xvvnp8j5jns6hi20r39agms54rrwqyr1918f";
   };
+
+  patches = [ (fetchpatch {
+    url = "https://github.com/arcnmx/zsh-tab-title/commit/273c4fa3be43687cf7202589aa11d1f5a3789b7f.patch";
+    sha256 = "035wiljxych4nydxkvffcb1czrxrvbw1g13qvwfafycqgdfv8zww";
+  }) ];
 }
