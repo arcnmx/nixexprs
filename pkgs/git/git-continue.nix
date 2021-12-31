7cb19eee51ed3e24925140dfefcab22a3268544b
arcnmx <arcnmx@users.noreply.github.com>
2021-12-30 20:00:57 -0800

    move git helpers out of repo

--- pkgs/git/git-continue.nix
+++ pkgs/git/git-continue.nix
@@ -1,16 +1,13 @@
-{ stdenvNoCC }: stdenvNoCC.mkDerivation {
+{ stdenvNoCC, fetchFromGitHub }: stdenvNoCC.mkDerivation rec {
   pname = "git-continue";
   version = "2021-12-11";
 
-  src = ./git-continue.sh;
-
-  unpackPhase = ''
-    runHook preUnpack
-
-    cp $src git-continue.sh
-
-    runHook postUnpack
-  '';
+  src = fetchFromGitHub {
+    owner = "arcnmx";
+    repo = pname;
+    rev = "f672a4edbacc129c97d5e9cafb3a562fee7ddd6a";
+    sha256 = "1vap78ncg9n0v6lbdc661115y68llga3a1ja5csbnq206y6950i4";
+  };
 
   installPhase = ''
     runHook preInstall
